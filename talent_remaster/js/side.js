63e33e828df0685b8a1270692d0b8ddc6184e36d
krosy1337 <krosy1337@gmail.com>
2021-06-12 12:54:11 +0300

    fix

--- talent_remaster/js/side.js
+++ talent_remaster/js/side.js
@@ -1,3 +1,19 @@
+function getScrollWidth() {
+	let div = document.createElement('div');
+
+	div.style.overflowY = 'scroll';
+	div.style.width = '50px';
+	div.style.height = '50px';
+
+	document.body.append(div);
+
+	let scrollWidth = div.offsetWidth - div.clientWidth;
+
+	div.remove();
+
+	return scrollWidth
+}
+
 function startSide() {
 	const side = document.querySelector('.side')
 
@@ -5,11 +21,16 @@ function startSide() {
 		const menuBtn = document.querySelector('.menu-btn')
 		const layout = document.querySelector('.layout')
 		const sideArrow = document.querySelector('.side__arrow')
+		const scrollWidth = getScrollWidth()
 
 		function exitHandler() {
 			side.classList.remove('active')
 			layout.classList.remove('active')
-			document.body.classList.remove('lock')
+			side.addEventListener('transitionend', () => {
+				document.body.classList.remove('lock')
+				document.body.style.paddingRight = ''
+			})
+
 
 			this.removeEventListener('click', exitHandler)
 		}
@@ -17,7 +38,11 @@ function startSide() {
 		menuBtn.addEventListener('click', () => {
 			side.classList.add('active')
 			layout.classList.add('active')
-			document.body.classList.add('lock')
+
+			side.addEventListener('transitionend', () => {
+				document.body.classList.add('lock')
+				document.body.style.paddingRight = `${scrollWidth}px`
+			})
 
 			sideArrow.addEventListener('click', exitHandler)
 			layout.addEventListener('click', exitHandler)
