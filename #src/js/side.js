b0506b64d35528456351d082b92b4fa642521181
krosy1337 <krosy1337@gmail.com>
2021-06-12 12:33:50 +0300

    add side animation

--- #src/js/side.js
+++ #src/js/side.js
@@ -9,6 +9,7 @@ function startSide() {
 		function exitHandler() {
 			side.classList.remove('active')
 			layout.classList.remove('active')
+			document.body.classList.remove('lock')
 
 			this.removeEventListener('click', exitHandler)
 		}
@@ -16,6 +17,7 @@ function startSide() {
 		menuBtn.addEventListener('click', () => {
 			side.classList.add('active')
 			layout.classList.add('active')
+			document.body.classList.add('lock')
 
 			sideArrow.addEventListener('click', exitHandler)
 			layout.addEventListener('click', exitHandler)
