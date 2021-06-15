975aa1a6177cd2a0bcbaa98694617d35fcd3a21c
krosy1337 <krosy1337@gmail.com>
2021-06-15 20:39:02 +0300

    add tooltip

--- #src/js/modal.js
+++ #src/js/modal.js
@@ -2,17 +2,20 @@ function startModal() {
 	const modalBtn = document.querySelector('.side__accounts-arrow')
 	const modal = document.querySelector('.modal')
 
-	modalBtn.addEventListener('click', () => {
-		modal.classList.add('active')
+	if (modal) {
+		modalBtn.addEventListener('click', () => {
+			modal.classList.add('active')
 
-		modal.addEventListener('click', event => {
-			const target = event.target
+			modal.addEventListener('click', event => {
+				const target = event.target
 
-			if (target.closest('.modal__close-btn') || target.classList.contains('modal__wrapper')) {
-				modal.classList.remove('active')
-			}
+				if (target.closest('.modal__close-btn') || target.classList.contains('modal__wrapper')) {
+					modal.classList.remove('active')
+				}
+			})
 		})
-	})
+	}
+	
 }
 
 export default startModal
